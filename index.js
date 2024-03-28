97e41c7b916cfc7e2ed593548462b5d205b053dd
audrey cherop <audreycherop06@gmail.com>
2024-03-28 23:32:40 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers,copy) {
+    return drivers.filter(driver => driver.toLowerCase() === copy.toLowerCase());
+}
+
+
+function fuzzyMatch(drivers,copy) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(copy.toLowerCase()));
+}
+
+function matchName(drivers, copy) {
+    return drivers.filter(driver => driver.name.toLowerCase() === copy.toLowerCase());
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const driverObjects = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Bobby', hometown: 'Tampa Bay' }
+];
